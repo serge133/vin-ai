8e612e66b1bd71db91f6a01943a9c211efd96314
Michael <michaelbatrakov@gmail.com>
2020-08-21 15:38:03 -0700

    STABLE BETA 0.2

--- executable.py
+++ executable.py
@@ -1,8 +1,9 @@
 import engine
 # from speechrecognition import ai_ask
 import printing
+import getpass
 
 # user_ask = ai_ask('What do you want me to do?', '')
-printing.ai_speak("What do you want to do?")
+printing.ai_speak(f"Hello {getpass.getuser()}, what do you want to do?")
 user_ask = str(printing.user_input())
 engine.ai(user_ask)
--- scripts/OSactions.py
+++ scripts/OSactions.py
@@ -1,9 +1,11 @@
 import os
+import getpass
 import shutil
 import printing
 from name_inference import name_inference_engine
 
-working_folder = '/Users/michaelbatrakov/Desktop/Python/AOSA/test'
+user = getpass.getuser()
+working_folder = f'/Users/{user}'
 
 # The engine always provides the lowercase sentence
 # This is where the name inference engine is used
@@ -15,40 +17,51 @@ def goodbye():
 # ? Each of these actions have one parameter called sentence
 # ? sentence is what the user said to trigger this function
 # ? It is helpful if you want to extract data from the sentence
+folder_locations = [
+  f'{working_folder}/Desktop', 
+  f'{working_folder}/Documents', 
+  f'{working_folder}/Desktop/Python', 
+  f'{working_folder}/Desktop/React Native/', 
+  f'{working_folder}/Desktop/React', 
+  f'{working_folder}/Desktop/Python/Aosa/test'
+]
 
 def create_folder(sentence):
+  printing.print_action('CREATING FOLDER')
   # folder_name = str(input("Name of the Folder: "))
   # folder_location = str(input('Where should this folder be? '))
-  name_inference_prewords = ['named', 'called']
   # Gets next element after the preword if exists
   
+  name_inference_prewords = ['named', 'called']
   name_inference = name_inference_engine(name_inference_prewords, sentence)
   
-  printing.print_action('CREATING FOLDER')
   curr_working_directory = os.getcwd()
   # If folder_to_create could not parse user's speech then a folder with a unique id is created
-  folder_locations = ['desktop', 'documents', 'python', 'react native', 'react']
-  
-  print(' - FOLDER LOCATIONS - ',
-    *folder_locations,
-    sep='\n'
-  )
+  index=0
+  for folder in folder_locations:
+    print(f'{index}) {folder}')
+    index+=1
+  # print(' - FOLDER LOCATIONS - ',
+  #   *folder_locations,
+  #   sep='\n'
+  # )
   # folder_location = ai_ask(f'Where to create the folder', '')
   printing.ai_speak("Where to create the folder?")
-  folder_location = str(printing.user_input())
+  option = int(printing.user_input())
+  folder_location = folder_locations[option]
   
   if folder_location in folder_locations:
     printing.print_action(f'CREATING A FOLDER IN "{folder_location}"')
-    printing.ai_speak("What is the name of the folder")
     folder_to_create = ''
     if name_inference:
       folder_to_create = name_inference
     else:
+      printing.ai_speak("What is the name of the folder")
       folder_to_create = str(printing.user_input())
 
     try:
-      os.mkdir(f'{working_folder}/{folder_to_create}')
-      printing.print_action(f"CREATED {folder_to_create}")
+      os.mkdir(f'{folder_location}/{folder_to_create}')
+      printing.print_action(f"CREATED {folder_to_create} FOLDER")
       goodbye()
     except:
       printing.print_error(f'Could not make folder "{folder_to_create}" :(')
@@ -57,33 +70,51 @@ def create_folder(sentence):
     printing.print_error('Incorrect Folder')
 # ------------------------------------------------------------------------------------
 def delete_folder(sentence):
-  trash_folder = '.trash'
-  printing.print_action(f"MOVING FOLDERS FROM {working_folder} TO TRASH")
-  files = os.listdir(working_folder)
-  print(
-    '- FOLDERS -',
-    *files,
-    sep='\n'
-  )
-  printing.ai_speak('What folder do you want to delete?')
+  printing.print_action("MOVING FOLDERS TO TRASH")
 
+  trash_folder = f'/Users/{user}/.aosa_trash'
   name_inference_prewords = ['called', 'named']
   name_inference = name_inference_engine(name_inference_prewords, sentence)
-  folder_to_delete = ''
-  if name_inference:
-    folder_to_delete = name_inference
-  else:
-    folder_to_delete = str(printing.user_input())
-  # if not folder_to_delete:
-  #   print_error('Folder not specified')
 
-  try:
-    # shutil.rmtree(f'{working_folder}/{folder_to_delete}')
-    shutil.move(f'{working_folder}/{folder_to_delete}', f'{working_folder}/{trash_folder}')
-    printing.print_action(f"DELETED {folder_to_delete}")
-    goodbye()
-  except:
-    printing.print_error(f'Could not delete {folder_to_delete} :(')
+
+  printing.ai_speak("Where to delete a folder?")
+  
+  index=0
+  for folder in folder_locations:
+    print(f'{index}) {folder}')
+    index+=1
+
+  option = int(printing.user_input())
+  folder_location = folder_locations[option]
+
+  if folder_location in folder_locations:
+    printing.print_action(f'DELETING FOLDER IN {folder_location}')
+
+    files = os.listdir(folder_location)
+    print(
+      '- FOLDERS -',
+      *files,
+      sep='\n'
+    )
+    printing.ai_speak('What folder do you want to delete?')
+
+    folder_to_delete = ''
+    if name_inference:
+      folder_to_delete = name_inference
+    else:
+      folder_to_delete = str(printing.user_input())
+    # if not folder_to_delete:
+    #   print_error('Folder not specified')
+
+    try:
+      # shutil.rmtree(f'{home_folder}/{folder_to_delete}')
+      shutil.move(f'{folder_location}/{folder_to_delete}', trash_folder)
+      printing.print_action(f"DELETED {folder_to_delete}")
+      goodbye()
+    except:
+      printing.print_error(f'Could not delete {folder_to_delete} :(')
+  else:
+    printing.print_error("Folder doesn't exist :(")
 
     
 
@@ -101,7 +132,7 @@ def move_folder():
 
 def rename_folder():
   folder_to_rename=ai_ask('What folder do you want me to rename?', "didn't work")
-  print(f'{home_folder}/{src}', f'{home_folder}/{src}', sep='\n')
+  print(f'{working_folder}/{src}', f'{working_folder}/{src}', sep='\n')
   # shutil.move(f'{home_folder}/{src}', f'{home_folder}/{src}')
 
 def make_a_note():
